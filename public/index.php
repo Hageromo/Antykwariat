c68949dbc883d95258904393686410cb487a9149
adamo <adamo980724@gmail.com>
2021-04-24 15:31:19 +0200

    Test

--- public/index.php
+++ public/index.php
@@ -16,7 +16,6 @@ if ($_SERVER['APP_DEBUG']) {
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$kernel1 = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
